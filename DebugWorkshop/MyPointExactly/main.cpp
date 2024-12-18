9ab603318fc97c1477b38074dc6d09aff471201a
unknown <yehu.1234z@gmail.com>
2024-12-18 19:31:50 +0200

    Part 3

--- DebugWorkshop/MyPointExactly/main.cpp
+++ DebugWorkshop/MyPointExactly/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	Point p1(1, 2);
-	Point p2;
+	Point p2;  // When the placement is done at compile time, the function we created is not used
 	p2 = p1;
 
 	std::cout << "p1=" << p1 << std::endl;
